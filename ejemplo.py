0aea8b4bed3fddc3dd4aa8d6e40c0294c30ec4e9
Angelo La Madrid <angelo.lamadrid@vallegrande.edu.pe>
2024-03-05 22:51:47 -0500

    🌟 Mejoras del reto propuesto Improving the old repositories 2024 challenge.

--- ejemplo.py
+++ ejemplo.py
@@ -3,24 +3,8 @@
 import hashlib
 
 
-usuarios_lista = ["angelo.la.madrid", "abel.huamani", "jefferson.mauricio", "gian.nuñez", "pedro.porras", "erick.portuguez", "jhon.sotomayor", "carlos.hernandez", "jonny.silverhand", "diego.sanchez", "valeria.ramirez"]
+usuarios_lista = ["naomi.camas","angelo.la.madrid", "abel.huamani", "jefferson.mauricio", "gian.nuñez", "pedro.porras", "erick.portuguez", "jhon.sotomayor", "carlos.hernandez", "jonny.silverhand", "diego.sanchez", "valeria.ramirez"]
 usuarios = {i+1: {"nombres": ' '.join(nombre.split('.')[:-1]), "apellidos": nombre.split('.')[-1]} for i, nombre in enumerate(usuarios_lista)}
-''' descomentar si es necesario codigo backup
-usuarios = {
-    1: {"nombres": "Naomi", "apellidos": "Cama"},
-    2: {"nombres": "Abel", "apellidos": "Huamani"},
-    3: {"nombres": "Angelo", "apellidos": "La Madrid"},
-    4: {"nombres": "Jefferson", "apellidos": "Mauricio"},
-    5: {"nombres": "Gian", "apellidos": "Nuñez"},
-    6: {"nombres": "Pedro", "apellidos": "Porras"},
-    7: {"nombres": "Erick", "apellidos": "Portuguez"},
-    8: {"nombres": "Jhon", "apellidos": "Sotomayor"},
-    9: {"nombres": "Carlos", "apellidos": "Hernandez"},
-    10: {"nombres": "Jonny", "apellidos": "Silverhand"},
-    11: {"nombres": "Diego", "apellidos": "Sanchez"},
-    12: {"nombres": "Valeria", "apellidos": "Ramirez"}
-}
-'''
 
 
 def elegir_usuario(mensaje):
@@ -33,13 +17,13 @@ def enviar_soles(id_origen, id_destino, soles):
 blockchain = []
 
 while True:
-    print("\n--- NUEVA TRANSACCIÓN ---")
+    print("\n--- NUEVA TRANSACCIÓN ---\n")
     print("Usuarios Disponibles:")
     for id, usuario in usuarios.items():
         print(f"{id} {usuario['nombres']} {usuario['apellidos']}")
-    id_origen = elegir_usuario("Elija un usuario que va a ENVIAR por su ID: ")
+    id_origen = elegir_usuario("\nElija un usuario que va a ENVIAR por su ID: ")
     id_destino = elegir_usuario("Elija un usuario que va a RECIBIR por su ID: ")
-    soles = float(input("Ingrese la cantidad de SOL COINs a enviar: "))
+    soles = float(input("\nIngrese la cantidad de SOL COINs a enviar: "))
     confirmacion = input(f"¿Está seguro de enviar {soles:.2f} SOL COINs a {usuarios[id_destino]['nombres']} {usuarios[id_destino]['apellidos']}? (S/N): ")
     if confirmacion.upper() == 'S':
         enviar_soles(id_origen, id_destino, soles)
@@ -56,3 +40,21 @@ while True:
 print("\n--- HISTORIAL DE TRANSACCIONES ---")
 for block_hash in blockchain:
     print(block_hash)
+
+
+''' descomentar si es necesario codigo backup
+usuarios = {
+    1: {"nombres": "Naomi", "apellidos": "Cama"},
+    2: {"nombres": "Abel", "apellidos": "Huamani"},
+    3: {"nombres": "Angelo", "apellidos": "La Madrid"},
+    4: {"nombres": "Jefferson", "apellidos": "Mauricio"},
+    5: {"nombres": "Gian", "apellidos": "Nuñez"},
+    6: {"nombres": "Pedro", "apellidos": "Porras"},
+    7: {"nombres": "Erick", "apellidos": "Portuguez"},
+    8: {"nombres": "Jhon", "apellidos": "Sotomayor"},
+    9: {"nombres": "Carlos", "apellidos": "Hernandez"},
+    10: {"nombres": "Jonny", "apellidos": "Silverhand"},
+    11: {"nombres": "Diego", "apellidos": "Sanchez"},
+    12: {"nombres": "Valeria", "apellidos": "Ramirez"}
+}
+'''
